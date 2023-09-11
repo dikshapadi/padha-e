5a3d1e6cbf0929140e309e8fd2602f6b94c3e6ba
aqdassajid <aqdassajid2@gmail.com>
2023-09-11 20:11:33 +0530

    commit2

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const collegeRoutes = require("./routes/college.js");
 
-dotenv.config({path: './config/config.env'})
-const app = express()
+dotenv.config({ path: "./config/config.env" });
+const app = express();
 app.use("/assets", express.static(__dirname + "/assets"));
 app.set("view engine", "ejs");
 // Routes
 app.use(collegeRoutes);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Sever running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+  )
+);
+// hello world!
