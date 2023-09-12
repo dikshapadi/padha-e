0294307fe65b06b3fa948cb6fa25ef4770bac65e
dikshapadi <dikshapadi@gmail.com>
2023-09-13 00:42:50 +0530

    small change

--- routes/college.js
+++ routes/college.js
@@ -1,6 +1,4 @@
-
 import express from "express";
-
 const router = express.Router();
 
 router.get('/college/dashboard',(req,res) =>{
--- routes/primary.js
+++ routes/primary.js
@@ -1,6 +1,4 @@
-
 import express from "express";
-
 const router = express.Router();
 
 router.get('/primary/dashboard',(req,res) =>{
