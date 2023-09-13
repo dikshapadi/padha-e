4ab76e3b27e0ba000bc28323471e3beeed1a4b20
Navya Malhotra <navya0807@gmail.com>
2023-09-13 19:36:07 +0530

    routes,aboutus and contact page

--- routes/home.js
+++ routes/home.js
@@ -6,5 +6,13 @@ router.get("/", (req,res) => {
 	res.render("home/main");
 });
 
+router.get('/home/contact', (req,res) => {
+	res.render("home/contact");
+});
+
+router.get('/home/aboutus', (req,res) => {
+	res.render("home/aboutus");
+});
+
 
 export default router;
