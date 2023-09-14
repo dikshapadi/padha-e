086b7936429c15a85915fe95cda9a6c4032e1041
aqdassajid <aqdassajid2@gmail.com>
2023-09-14 17:10:01 +0530

    new mentor dashboard

--- routes/mentor.js
+++ routes/mentor.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/mentor/dashboard',(req,res) =>{
-    res.render("mentor/dashboard");
+router.get("/mentor/dashboard", (req, res) => {
+  res.render("mentor/dashboard");
 });
-
+router.get("/mentor/dashboardold", (req, res) => {
+  res.render("mentor/dashboardold");
+});
 
 export default router;
