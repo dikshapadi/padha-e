68992ddaa07678dc810b204e6129584bd54768f9
dikshapadi <dikshapadi@gmail.com>
2023-09-15 09:26:30 +0530

    added reminder to primary

---
+++ assets/js/reminder.js
@@ -0,0 +1,24 @@
+// Get references to HTML elements
+const popup = document.getElementById("screenTimePopup");
+const closePopupButton = document.getElementById("closePopup");
+
+function showPopup() {
+    popup.style.display = "block";
+    overlay.style.display = "block";
+  }
+  
+// Function to show the popup
+function showPopup() {
+  popup.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  popup.style.display = "none";
+}
+
+// Show the popup after a certain time (e.g., 30 minutes)
+const popupTimer = setInterval(showPopup, 10000); // 30 minutes in milliseconds
+
+// Close the popup when the close button is clicked
+closePopupButton.addEventListener("click", closePopup);
